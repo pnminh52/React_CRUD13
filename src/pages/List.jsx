66bc0ebc4ae9bce299fb7df1b8cb2b8e4e155210
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2024-12-19 23:12:30 +0700

    master

---
+++ src/pages/List.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const List = (props) => {
+  const { products, onDel } = props;
+  return (
+    <>
+      <div>
+        <Link to={`/products/add`}>
+          <button>Add products</button>
+        </Link>
+      </div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <td>name</td>
+              <td>price</td>
+              <td>des</td>
+              <td>img</td>
+              <td>action</td>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => {
+              return (
+                <tr key={product.id}>
+                  <td>{product.name}</td>
+                  <td>{product.price}</td>
+                  <td>{product.des}</td>
+                  <td>
+                    <img src={product.img} alt="" />
+                  </td>
+                  <td>
+                    <button onClick={() => onDel(product.id)}>Xoa</button>
+                    <Link to={`/products/${product.id}/update`}>
+                      <button>Update</button>
+                    </Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default List;
